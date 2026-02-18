93127bae6a933efeec210999a07ed89daddadeec
HASSAN <mimchenadira26@gmail.com>
2026-02-18 11:49:15 +0100

    backend vercelcorrectiondes import

--- backend/routes/statsRoutes.js
+++ backend/routes/statsRoutes.js
@@ -53,21 +53,3 @@ export default router;
 
 
 
-
-
-// import express from 'express';
-// // import AuthController from '../controllers/AuthController.js';
-// import ProfileController from '../controllers/profileController.js';
-// import AuthController from '../controllers/AuthController.js';
-
-// const router = express.Router();
-
-// // Toutes les routes nécessitent une authentification
-// router.use(AuthController.verifierToken);
-
-// // Routes de profil
-// router.get('/profil', ProfileController.obtenirProfil);
-// router.put('/profil', ProfileController.mettreAJourProfil);
-// router.post('/changer-mot-de-passe', ProfileController.changerMotDePasse);
-
-// export default router;
