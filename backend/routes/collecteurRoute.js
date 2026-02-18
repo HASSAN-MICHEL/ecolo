9dc4cf2f066c01b8ff4d045d66d3e7ba72b933c0
HASSAN <mimchenadira26@gmail.com>
2026-02-18 20:00:07 +0100

    lance gestion fichier collecteur 234R

--- backend/routes/collecteurRoute.js
+++ backend/routes/collecteurRoute.js
@@ -51,14 +51,14 @@
 
 import express from 'express';
 import CollecteurController from '../controllers/CollecteurController.js';
- import { uploadCollecteurFiles } from '../middleware/uploads.js';
+
 import { uploadCollecteurFiles, processUploads } from '../middleware/uploadToSupabase.js';
 import  AuthCollecteur  from '../controllers/AuthController.js';
 
 const router = express.Router();
 
 // Route d'inscription avec upload de fichiers
-// router.post('/inscription', uploadCollecteurFiles, CollecteurController.inscription);
+// router.post('/inscription', uploadCollecteurFiles, CollecteurController.inscription); // import { uploadCollecteurFiles } from '../middleware/uploads.js';
 
 router.post('/inscription', 
     uploadCollecteurFiles,    // Multer en mémoire
