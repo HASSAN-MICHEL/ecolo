18e3d879f4b3212cb7388aa8c4633d9dfcd0017f
HASSAN <mimchenadira26@gmail.com>
2026-02-18 11:43:42 +0100

    backend vercel92fADFqvcnV

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import AuthController from '../controllers/authController.js';
+import AuthController from '../controllers/AuthController.js';
 import ProfileController from '../controllers/profileController.js';
 
 const router = express.Router();
